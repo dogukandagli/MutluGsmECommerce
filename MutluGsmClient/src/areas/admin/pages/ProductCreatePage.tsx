a58c662e2b335d503f539d380fa1845b1bc22455
Dogukan Dagli <daglidogukan08@gmail.com>
2025-09-17 23:03:19 +0300

    .

--- MutluGsmClient/src/areas/admin/pages/ProductCreatePage.tsx
+++ MutluGsmClient/src/areas/admin/pages/ProductCreatePage.tsx
@@ -1,7 +1,19 @@
-import { Container } from "@mui/material";
+import { Container, TextField, Typography } from "@mui/material";
 import ProductCreateStepper from "../../../features/products/components/ProductCreateStepper";
+import Grid from "@mui/material/Grid";
+import { Box, Stack } from "@mui/system";
+import { useForm } from "react-hook-form";
 
 export default function ProductCreatePage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm({
+    defaultValues: {
+      name,
+    },
+  });
   return (
     <Container maxWidth="lg" sx={{ py: 6 }}>
       <ProductCreateStepper
@@ -9,7 +21,31 @@ export default function ProductCreatePage() {
         renderStep={(i) => {
           switch (i) {
             case 0:
-              return <>/* Product Info form alanların buraya */</>;
+              return (
+                <>
+                  <Box>
+                    <Typography variant="h6" fontWeight={700} mb={2}>
+                      Ürün Bilgisi
+                    </Typography>
+
+                    <Grid container spacing={3}>
+                      <Grid size={{ xs: 12, md: 6 }}>
+                        <Stack spacing={2}>
+                          <TextField
+                            {...register("name")}
+                            label="name"
+                            variant="standard"
+                            fullWidth
+                            autoFocus
+                            required
+                          ></TextField>
+                        </Stack>
+                      </Grid>
+                      <Grid size={{ xs: 12, md: 6 }}></Grid>
+                    </Grid>
+                  </Box>
+                </>
+              );
             case 1:
               return <>/* Media upload alanların buraya */</>;
             case 2:
--- MutluGsmClient/src/areas/admin/pages/ProductsPage.tsx
+++ MutluGsmClient/src/areas/admin/pages/ProductsPage.tsx
@@ -1,5 +1,10 @@
+import Container from "@mui/material/Container";
 import ProductsDataGrid from "../../../features/products/components/ProductsDatagrid";
 
 export default function ProductsPage() {
-  return <ProductsDataGrid />;
+  return (
+    <Container maxWidth="lg" sx={{ py: 6 }}>
+      <ProductsDataGrid />
+    </Container>
+  );
 }
